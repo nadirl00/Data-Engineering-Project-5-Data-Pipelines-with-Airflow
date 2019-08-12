dafef8f58a6bc81b3135388a0242e80f66551060
Nadir Lounnas <33787299+nadirl00@users.noreply.github.com>
2019-08-11 20:21:26 -0400

    Added complete operator implementation
    Replaced operator template with implementation

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -1,3 +1,4 @@
+from airflow.contrib.hooks.aws_hook import AwsHook
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
@@ -8,15 +9,25 @@ class LoadDimensionOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 table = "",
+                 stl_statement = "",
+                 #missing target table
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id=redshift_conn_id
+        self.stl_statement = stl_statement
+        self.table=table
 
     def execute(self, context):
         self.log.info('LoadDimensionOperator not implemented yet')
+        #dimension must be emptied before a load
+
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        self.log.info("DELETE FROM {}".format(self.table))
+        redshift.run("DELETE FROM {}".format(self.table))
+        completeSql = "INSERT INTO {} ".format(self.table) + self.stl_statement
+        self.log.info("Inserting to dim table")
+        redshift.run(completeSql)
