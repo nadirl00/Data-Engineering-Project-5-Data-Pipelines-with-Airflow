cf3b8a4feda518a16619ae7f33f9c385dd8b60c5
Nadir Lounnas <33787299+nadirl00@users.noreply.github.com>
2019-08-11 20:20:16 -0400

    Added operator implementation
    Added operator implementation

--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -1,3 +1,4 @@
+from airflow.contrib.hooks.aws_hook import AwsHook
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
@@ -8,15 +9,23 @@ class LoadFactOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 aws_credentials_id="",
+                 stl_statement="",
+                 #destination table is always the same. Does not need to be specified.
                  *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id=redshift_conn_id
+        self.aws_credentials_id=aws_credentials_id
+        self.stl_statement=stl_statement
 
     def execute(self, context):
-        self.log.info('LoadFactOperator not implemented yet')
+        self.log.info('Inserting into fact table')
+        
+
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)        
+        # a simple insert is required here
+        completeSql = "INSERT INTO public.songplays " + self.stl_statement
+        self.log.info(completeSql)
+        redshift.run(completeSql)
